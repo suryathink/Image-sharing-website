578917be7f08aad2ac45b3e71a800a41f8f666a7
Surya Prakash Singh <suryaprakashsingh110@gmail.com>
2024-10-14 20:55:12 +0530

    Update authorization.js

--- server/middlewares/authorization.js
+++ server/middlewares/authorization.js
@@ -1,54 +1,52 @@
-const dotenv = require("dotenv");
-dotenv.config();
-const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
-const blacklistTokenData = require("../models/blacklist");
+const dotenv = require('dotenv')
+dotenv.config()
+const jwt = require('jsonwebtoken') // Unused variable
+const User = require('../models/userModel')
+const blacklistTokenData = require('../models/blacklist')
 
 const {
-  signup,
-  login,
+  signup, // Unused variable
+  login,  // Unused variable
   verifyToken,
   getUserById,
-  addImage,
-} = require("../controllers/authController");
+  addImage // Unused variable
+} = require('../controllers/authController')
 
 async function authMiddleware(req, res, next) {
   try {
-    const headers = req.headers;
+    const headers=req.headers  // No space around `=`
 
-    const authHeader = headers["authorization"];
+    const authHeader = headers['authorization'] // Inconsistent quote style
 
     if (authHeader) {
-      const token = authHeader.split(" ").pop();
+      const token = authHeader.split(' ').pop()
 
       // blacklist code
-      let blacklistedToken = await blacklistTokenData.findOne({ token });
-      if (blacklistedToken) {
-        return res.status(401).json({ message: "Unauthorized, Login Again" });
-      }
+      let blacklistedToken = await blacklistTokenData.findOne({ token })
+      if (blacklistedToken)
+        return res.status(401).json({ message: 'Unauthorized, Login Again' }) // Single-line if statement might violate rules
+
       // blacklist code ends here
 
-      const payload = verifyToken(token);
+      const payload = verifyToken(token)
 
-      const user = await getUserById(payload._id);
+      const user = await getUserById(payload._id)
 
-      req.loggedInUser = user;
+      req.loggedInUser = user
 
-      next();
+      next() // Missing semicolon
     } else {
       return res.status(400).send({
-        message: "User is not logged in",
-      });
+        message: 'User is not logged in'
+      })
     }
-
-    // mkkd
   } catch (err) {
-    console.error(err);
+    console.error(err)
 
     return res.status(500).send({
-      error: "Something went wrong",
-    });
+      error: 'Something went wrong'
+    })
   }
 }
 
-module.exports = authMiddleware;
+module.exports = authMiddleware // Missing semicolon
